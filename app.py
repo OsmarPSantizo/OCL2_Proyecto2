9e070ffa803b55c20efd9efe53b2e396b1cde14f
OsmarPSantizo <69328909+OsmarPSantizo@users.noreply.github.com>
2021-12-28 16:59:55 -0600

    Prueba heroku

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 from flask.helpers import send_from_directory;
 from flask_cors import CORS, cross_origin;
 
 
-app = Flask(__name__,static_folder='my-app/build', static_url_path='')
-cors = CORS(app)
+
+app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/api',methods=['GET'])
 @cross_origin()
