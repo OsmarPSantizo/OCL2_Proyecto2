6b98b10a4d8390ea3b2e89e4a8c6bf2f469d3980
OsmarPSantizo <69328909+OsmarPSantizo@users.noreply.github.com>
2021-12-28 23:18:12 -0600

    prueba de cambios

--- my-app/src/Component/Deploy/Deploy.js
+++ my-app/src/Component/Deploy/Deploy.js
@@ -1,5 +1,20 @@
 import React from 'react'
 
 export const Deploy = ({prop}) => {
-    return <div>{prop.tutorial}</div>
+    return(
+        
+<div className="dd-wrapper">
+  <div className="dd-header">
+    <div className="dd-header-title"></div>
+    <div>{prop.tutorial}</div>
+  </div>
+  <div className="dd-list">
+    <button className="dd-list-item"></button>
+    <button className="dd-list-item"></button>
+    <button className="dd-list-item"></button>
+  </div>
+</div>
+
+        
+    ) 
 }
